7394deb403a46c8b45c17fb02b21514ea12c1a36
Abylay <boranbayev.abylay@gmail.com>
2025-08-07 12:53:23 +0500

    fetch data from TMDB

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,8 +1,19 @@
 import React from 'react'
 
-function Search() {
+function Search({ searchTerm, setSearchTerm }) {
     return (
-        <div>Search</div>
+        <div className="search">
+            <div>
+                <img src="/search.svg" alt="search"/>
+
+                <input
+                    type="text"
+                    placeholder="Search through thousands of movies"
+                    value={searchTerm}
+                    onChange={(e) => setSearchTerm(e.target.value)}
+                />
+            </div>
+        </div>
     )
 }
 
